73a93b3c50cbd5ee74989924147be042e09b8313
moodyomar <lonak.secret@gmail.com>
2021-11-20 17:23:26 +0200

    add date calc

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
   let [hrs,setHrs] = useState(0)
   let [mins,setMins] = useState(0)
-  let [date,setDate] = useState('')
+  let [date,setDate] = useState(new Date())
+  let [recipe,setRecipe] = useState(0.15)
   let [plazmaDate,setPlazmaDate] = useState('---')
 
-  const onChange = (e) => {
-    console.log(e.target.value);
-    if(e.target.name === 'hours'){
-      setHrs(Math.floor(e.target.value * 0.15).toFixed(1));
-      setMins(Math.floor((e.target.value * 0.15) * 60).toFixed(1));
-    }
-    if(e.target.name === 'date') setDate(e.target.value);
-    switch (e.target.value) {
-      case 'v9':
-      console.log('hey v9');
-        break;
+  function addHours(date, hours) {
+    const newDate = new Date(date);
+    newDate.setHours(newDate.getHours() + hours);
+    return newDate;
+  }
+    useEffect(() => {
+      console.log('hrs ',hrs);
+      console.log('recipe ',recipe);
+      
+      let d = addHours(date,Number(hrs)).toString();
+      console.log('d ',d);
+      
+      setPlazmaDate(d.slice(0,d.indexOf('G')));
+  
+  },[recipe,hrs,date])
 
-      case 'v10':
-      console.log(date);
-        break;
-    
-      default:
-        break;
+  const onChange = (e) => {
+    if(e.target.name === 'hours'){
+      setHrs(Math.floor(e.target.value * recipe).toFixed(1));
+      setMins(Math.floor((e.target.value * recipe) * 60).toFixed(1));
+    }
+    if(e.target.name === 'date') setDate(new Date(e.target.value));
+    if(e.target.name === 'recipes'){ 
+      switch (e.target.value) {
+        case 'v9':
+          setRecipe(0.15);
+          break;
+  
+        case 'v10':
+          setRecipe(0.10);
+          break;
+      
+        default:
+          break;
+      }
     }
   }
 
@@ -45,7 +63,7 @@ function App() {
       <div className="d-flex mb-4 h2 justify-content-center">
         חישוב שעות פלזמה
       </div>
-     <div className="result mb-5 mx-auto">
+     <div className="result mb-3 mx-auto">
     <div className="d-flex justify-content-around bg-light text-dark">
     <label  htmlFor="total-hours">סך הכל שעות</label>
     <label htmlFor="total-minutes">סך הכל דקות</label>
@@ -58,18 +76,12 @@ function App() {
     <label htmlFor="plazma-date">הפלאזמה תסתיים ביום</label>
     </div>
     <div className="d-flex justify-content-around">
-    <label className="h3 mt-2" htmlFor="plazma-date">{plazmaDate}</label>
+      {hrs &&
+    <label className="h4 mt-2" htmlFor="plazma-date">{plazmaDate}</label>
+      }
     </div>
   </div>
-     <div className="d-flex justify-content-between mb-2">
-       <label htmlFor="date" id="date">מתי הוכנסה לפלזמה</label>
-       <label htmlFor="hours">שעות גידול</label>
-     </div>
-     <div className="d-flex justify-content-between">
-       <input type="datetime-local" className="form-control w-50" name="date" id="date" onChange={e => onChange(e)} />
-       <input type="tel" name="hours" className="form-control w-25" id="hours" onChange={e => onChange(e)} />
-     </div>
-     <div className="d-flex justify-content-around mt-5 align-items-center">
+  <div className="d-flex justify-content-around my-4 align-items-center">
        <div className="d-flex">
        <label htmlFor="v9">V9</label>
        <input type="radio" name="recipes" value="v9" id="v9" defaultChecked="checked" onChange={e => onChange(e)}/>
@@ -79,6 +91,16 @@ function App() {
        <input type="radio" name="recipes" value="v10" id="v10" onChange={e => onChange(e)}/>
        </div>
      </div>
+
+     <div className="d-flex justify-content-between mb-2">
+       <label htmlFor="hours">שעות גידול</label>
+       <label htmlFor="date" id="date">מתי הוכנסה לפלזמה</label>
+     </div>
+     <div className="d-flex justify-content-between">
+       <input type="tel" name="hours" className="form-control w-25" id="hours" onChange={e => onChange(e)} />
+       <input type="datetime-local" className="form-control w-50" name="date" id="date" onChange={e => onChange(e)} />
+     </div>
+     
   <div className="d-flex justify-content-center mt-4">
   <button className="btn btn-light">איפוס הכל</button>
   </div>
